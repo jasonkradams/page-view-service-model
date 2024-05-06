1aa25b06003aa543dcd5c907738b7897fad89314
Jason Adams <j.adams@f5.com>
2024-05-05 21:17:57 -0700

    Update module to Java 21

--- src/main/java/guru/springframework/model/events/PageViewEvent.java
+++ src/main/java/guru/springframework/model/events/PageViewEvent.java
@@ -1,7 +1,8 @@
 package guru.springframework.model.events;
 
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlRootElement;
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlRootElement;
+
 import java.util.Date;
 
 /**
